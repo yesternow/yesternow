77d0f2c3b878ff4d48a5c0765d94e942755e6842
Mark B <markallenblanchard@gmail.com>
2018-10-31 15:00:12 -0500

    made changes to user in store and component and implemented OAuth

--- client/store/index.js
+++ client/store/index.js
@@ -4,7 +4,7 @@ import thunkMiddleware from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import user from './user';
 import product from './product';
-import users from '/user';
+import users from './users';
 
 const reducer = combineReducers({user, users, product});
 const middleware = composeWithDevTools(applyMiddleware(thunkMiddleware, createLogger({collapsed: true})));
--- server/auth/google.js
+++ server/auth/google.js
@@ -1,8 +1,9 @@
-const passport = require('passport')
-const router = require('express').Router()
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
-const {User} = require('../db/models')
-module.exports = router
+const passport = require('passport');
+const router = require('express').Router();
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const {User} = require('../db/models');
+const googleConfigSecret = require('../../secrets');
+module.exports = router;
 
 /**
  * For OAuth keys and other secrets, your Node process will search
@@ -19,39 +20,32 @@ module.exports = router
  */
 
 if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
-  console.log('Google client ID / secret not found. Skipping Google OAuth.')
+	console.log('Google client ID / secret not found. Skipping Google OAuth.');
 } else {
-  const googleConfig = {
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK
-  }
+	const googleConfig = googleConfigSecret;
 
-  const strategy = new GoogleStrategy(
-    googleConfig,
-    (token, refreshToken, profile, done) => {
-      const googleId = profile.id
-      const name = profile.displayName
-      const email = profile.emails[0].value
+	const strategy = new GoogleStrategy(googleConfig, (token, refreshToken, profile, done) => {
+		const googleId = profile.id;
+		const name = profile.displayName;
+		const email = profile.emails[0].value;
 
-      User.findOrCreate({
-        where: {googleId},
-        defaults: {name, email}
-      })
-        .then(([user]) => done(null, user))
-        .catch(done)
-    }
-  )
+		User.findOrCreate({
+			where: {googleId},
+			defaults: {name, email}
+		})
+			.then(([ user ]) => done(null, user))
+			.catch(done);
+	});
 
-  passport.use(strategy)
+	passport.use(strategy);
 
-  router.get('/', passport.authenticate('google', {scope: 'email'}))
+	router.get('/', passport.authenticate('google', {scope: 'email'}));
 
-  router.get(
-    '/callback',
-    passport.authenticate('google', {
-      successRedirect: '/home',
-      failureRedirect: '/login'
-    })
-  )
+	router.get(
+		'/callback',
+		passport.authenticate('google', {
+			successRedirect: '/home',
+			failureRedirect: '/login'
+		})
+	);
 }

567a4b27a2a60e953e60ea36b57a596cc4ca588d
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2018-10-31 10:13:20 -0500

    user router

--- server/api/users.js
+++ server/api/users.js
@@ -1,6 +1,6 @@
-const router = require('express').Router()
-const {User} = require('../db/models')
-module.exports = router
+const router = require('express').Router();
+const { User } = require('../db/models');
+module.exports = router;
 
 router.get('/', async (req, res, next) => {
   try {
@@ -8,10 +8,54 @@ router.get('/', async (req, res, next) => {
       // explicitly select only the id and email fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'email']
-    })
-    res.json(users)
+      attributes: ['id', 'email'],
+    });
+    res.json(users);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
+
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const user = User.findById(req.params.userId, {
+      attributes: ['id', 'firstName', 'lastName', 'email'],
+    });
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//create user
+
+router.post('/', async (req, res, next) => {
+  try {
+    const user = await User.create(req.body, {
+      where: {
+        isAdmin: true,
+      },
+    });
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//update user
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) return res.sendStatus(404);
+    const updatedUser = await user.update(req.body, {
+      where: {
+        isAdmin: true,
+      },
+    });
+    res.json(updatedUser);
+  } catch (err) {
+    next(err);
+  }
+});

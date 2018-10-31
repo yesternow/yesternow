679cb6a85262067e7860cf05b96851f6645e6682
Mark B <markallenblanchard@gmail.com>
2018-10-31 10:46:02 -0500

    updated Tests for Product Model

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -6,6 +6,7 @@ const db = require('../db');
 const app = require('../index');
 const User = db.model('user');
 
+//needs to be updated
 xdescribe('User routes', () => {
 	beforeEach(() => {
 		return db.sync({force: true});
--- server/db/models/product.spec.js
+++ server/db/models/product.spec.js
@@ -2,6 +2,7 @@ const {expect} = require('chai');
 const db = require('../index');
 const Product = db.model('product');
 
+//test
 describe('Product model', () => {
 	beforeEach(() => {
 		return db.sync({force: true});

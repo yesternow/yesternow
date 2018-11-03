9f8402a61a3b237812194a5b7b1a7d2f62974699
Joseph Park <josephpaulpark@gmail.com>
2018-11-02 22:59:53 -0500

    add product page working

--- client/components/addproduct.js
+++ client/components/addproduct.js
@@ -1,115 +1,201 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { addProduct } from '../store';
-import { Button, Form, Container } from 'semantic-ui-react';
-import history from '../history'
+import { Button, Form, Container, Grid, Header } from 'semantic-ui-react';
+import history from '../history';
 class AddProduct extends Component {
   constructor() {
     super();
+    this.state = {
+      title: '',
+      description: '',
+      price: '',
+      quantity: '',
+      weight: '',
+      brand: '',
+      imageUrl: '',
+      dimensions: '',
+    };
 
+    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
   }
 
   handleSubmit(event) {
     event.preventDefault();
+    this.props
+      .addProduct(this.state)
+      .then(action => history.push(`/products/${action.product.id}`));
 
-    const title = event.target.title.value;
-    const description = event.target.description.value;
-    const price = Number(event.target.price.value);
-    const quantity = Number(event.target.quantity.value);
-    const weight = Number(event.target.weight.value);
-    const brand = event.target.brand.value;
-    const imageUrl = event.target.imageUrl.value;
+    this.setState({
+      title: '',
+      description: '',
+      price: '',
+      quantity: '',
+      weight: '',
+      brand: '',
+      imageUrl: '',
+      dimensions: '',
+    });
+  }
 
-    this.props.addProduct({
+  render() {
+    const {
       title,
       description,
       price,
       quantity,
       weight,
       brand,
-    }).then(response => history.push(`/products/${response.product.id}`))
-    }
+      imageUrl,
+      dimensions,
+    } = this.state;
 
-  render() {
     return (
       <Container>
-        <h3>Add New Product</h3>
+        <Header as="h3">Add New Product</Header>
         <Form onSubmit={this.handleSubmit}>
-          <Form.Field>
-            <div>
-              <label>Product Title</label>
-              <input
-                placeholder="Product Title"
-                id="title"
-                type="text"
-                required
-              />
-            </div>
+          <Form.Field required>
+            <label>Product Title</label>
+            <input
+              placeholder="Product Title"
+              name="title"
+              value={title}
+              type="text"
+              onChange={this.handleChange}
+            />
           </Form.Field>
-          <Form.Field>
-            <div>
-              <label>Product Description</label>
-              <input
-                placeholder="Product Description"
-                name="description"
-                type="text"
-                required
-              />
-            </div>
+          <Form.Field required>
+            <label>Product Description</label>
+            <input
+              placeholder="Product Description"
+              name="description"
+              value={description}
+              type="text"
+              onChange={this.handleChange}
+            />
           </Form.Field>
-          <Form.Field>
-            <div>
+          <Form.Group widths="equal">
+            <Form.Field required>
               <label>Product Price</label>
               <input
                 placeholder="Product Price"
                 name="price"
+                min="0"
+                value={price}
                 type="number"
-                required
+                onChange={this.handleChange}
               />
-            </div>
-          </Form.Field>
-          <Form.Field>
-            <div>
+            </Form.Field>
+            <Form.Field required>
               <label>Product Quantity</label>
               <input
                 placeholder="Product Quantity"
                 name="quantity"
+                min="0"
+                value={quantity}
                 type="number"
+                onChange={this.handleChange}
                 required
               />
-            </div>
-          </Form.Field>
-          <Form.Field>
-            <div>
+            </Form.Field>
+            <Form.Field required>
               <label>Product Weight</label>
               <input
                 placeholder="Product Weight in Ounces"
                 name="weight"
+                value={weight}
+                min="0"
                 type="number"
-                required
+                onChange={this.handleChange}
               />
-            </div>
-          </Form.Field>
-          <Form.Field>
-            <div>
-              <label>Product Brand</label>
-              <input placeholder="Product Brand" name="brand" type="text" />
-            </div>
-          </Form.Field>
-          <Form.Field>
-            <div>
-              <label>Product Image</label>
+            </Form.Field>
+            <Form.Field>
+              <label>Product Dimensions</label>
               <input
-                placeholder="Image Url"
-                name="imageUrl"
-                type="url"
+                placeholder="Product Dimensions"
+                name="dimensions"
+                value={dimensions}
+                type="text"
+                onChange={this.handleChange}
               />
-            </div>
+            </Form.Field>
+          </Form.Group>
+          <Form.Field>
+            <label>Product Brand</label>
+            <input
+              placeholder="Product Brand"
+              name="brand"
+              type="text"
+              onChange={this.handleChange}
+              value={brand}
+            />
           </Form.Field>
-          <div>
-            <Button type="submit">Submit</Button>
-          </div>
+          <Form.Field>
+            <label>Product Image</label>
+            <input
+              placeholder="Image Url"
+              name="imageUrl"
+              value={imageUrl}
+              type="url"
+              onChange={this.handleChange}
+            />
+          </Form.Field>
+          {/* <Form.Group>
+            <label>Is Active</label>
+            <Form.Radio
+              label="Yes"
+              name="isActive"
+              value={isActive}
+              checked={isActive === true}
+              onChange={this.handleChange}
+            />
+            <Form.Radio
+              label="No"
+              name="isActive"
+              value={isActive}
+              checked={isActive === false}
+              onChange={this.handleChange}
+            />
+          </Form.Group> */}
+          {/* <Form.Group>
+            <label>Is Available</label>
+            <Form.Radio
+              label="Yes"
+              value={isAvailable}
+              checked={isAvailable === true}
+              onChange={this.handleChange}
+            />
+            <Form.Radio
+              label="No"
+              name="isAvailable"
+              value={isAvailable}
+              checked={isAvailable === false}
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+          <Form.Group>
+            <label>Is Featured</label>
+            <Form.Radio
+              label="Yes"
+              name="isFeatured"
+              value={isFeatured}
+              checked={isFeatured === true}
+              onChange={this.handleChange}
+            />
+            <Form.Radio
+              label="No"
+              name="isAvailable"
+              value={isFeatured}
+              checked={isFeatured === false}
+              onChange={this.handleChange}
+            />
+          </Form.Group> */}
+          <Button type="submit">Submit</Button>
         </Form>
       </Container>
     );

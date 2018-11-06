30e9762060aaa30c9cddaaa156907ac8a3d6a481
Joseph Park <josephpaulpark@gmail.com>
2018-11-06 10:32:36 -0600

    fixing styling

--- client/components/singleProduct.js
+++ client/components/singleProduct.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchProduct } from '../store';
-import { Button, Container, Image, Item, Icon, Grid } from 'semantic-ui-react';
+import { fetchProduct, sendAddToCart } from '../store';
+import {
+  Button,
+  Container,
+  Image,
+  Item,
+  Icon,
+  Grid,
+  Divider,
+  Rating,
+} from 'semantic-ui-react';
 export class SingleProduct extends Component {
   componentDidMount() {
     this.props.loadProduct(this.props.match.params.id);
   }
   render() {
     const {
+      id,
       title,
       description,
       price,
@@ -19,32 +29,82 @@ export class SingleProduct extends Component {
       brand,
     } = this.props.product;
 
+    if (!this.props.product) {
+      return <div>No product available</div>;
+    }
     return (
-      <Container>
+      <Container border="3px">
         <Grid>
-          <Grid.Column width={6}>
-            <Image.Group size="medium">
-              {images &&
-                images.map(image => (
-                  <Image key={image.id} src={image.imageUrl} bordered />
-                ))}
-            </Image.Group>
-          </Grid.Column>
+          <Grid.Row />
+          <Grid.Row>
+            <Grid.Column width={2} />
+            <Grid.Column width={12}>
+              <Image.Group size="medium">
+                {images &&
+                  images.map(image => (
+                    <Image key={image.id} src={image.imageUrl} bordered />
+                  ))}
+                <Divider hidden />
+              </Image.Group>
+            </Grid.Column>
+            <Grid.Column width={2} />
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={2} />
+            <Grid.Column width={12}>
+              <Item>
+                <Item.Content>
+                  <Item.Header as="h3">{title}</Item.Header>
+                  <Item.Meta>${(price / 100).toFixed(2)}</Item.Meta>
+                  <Item.Meta>
+                    {quantity < 5 && `Only ${quantity} left!`}
+                  </Item.Meta>
 
-          <Grid.Column width={10}>
-            <Item>
-              <Item.Content>
-                <Item.Header>{title}</Item.Header>
-                <Item.Meta>{price}</Item.Meta>
-                <Item.Description>{description}</Item.Description>
-                <Item.Extra>
-                  <Button floated="right" color="orange">
-                    <Icon name="shop" />
-                  </Button>
-                </Item.Extra>
-              </Item.Content>
-            </Item>
-          </Grid.Column>
+                  <Item.Description>{description}</Item.Description>
+                  <Item.Extra>Dimensions: {dimensions}</Item.Extra>
+                  <Item.Extra>
+                    <Button
+                      floated="right"
+                      onClick={() =>
+                        this.props.sendAddToCart({
+                          quantity: 1,
+                          productId: id,
+                          cartId: this.props.cartId,
+                        })
+                      }
+                      color="orange"
+                    >
+                      <Icon name="shop" />
+                    </Button>
+                  </Item.Extra>
+                </Item.Content>
+              </Item>
+            </Grid.Column>
+            <Grid.Column width={2} />
+          </Grid.Row>
+          <Grid.Row>
+            <Divider />
+            <Grid.Column width={2} />
+            <Grid.Column width={12}>
+              <Divider />
+              <h3>Reviews</h3>
+              {reviews &&
+                reviews.map(review => (
+                  <Item key={review.id}>
+                    <Rating
+                      icon="star"
+                      defaultRating={review.rating}
+                      maxRating={5}
+                    />
+                    <Item.Description>{review.description}</Item.Description>
+                  </Item>
+                ))}
+              <Button floated="right" color="orange">
+                Leave Review
+              </Button>
+            </Grid.Column>
+            <Grid.Column width={2} />
+          </Grid.Row>
         </Grid>
       </Container>
     );
@@ -53,11 +113,12 @@ export class SingleProduct extends Component {
 
 const mapStateToProps = state => ({
   product: state.product.product,
+  cartId: state.carts.cart.id,
 });
 
 const mapDispatchToProps = dispatch => ({
   loadProduct: productId => dispatch(fetchProduct(productId)),
-
+  sendAddToCart: product => dispatch(sendAddToCart(product)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(SingleProduct);

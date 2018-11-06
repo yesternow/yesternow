d295e5d7a3a448da2d325b9c571d2e6dcedc66e9
Mustafa Dane <mustafadane@gmail.com>
2018-11-06 00:27:05 -0600

    total price calculation works and updates when quantity is updated

--- client/components/singleCartItem.js
+++ client/components/singleCartItem.js
@@ -1,60 +1,78 @@
-// import React, { Component } from 'react'
-// import { connect } from 'react-redux'
-// import { fetchCart, removeCartItem, sendAddToCart } from '../store';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { removeCartItem, sendAddToCart } from '../store';
+import { List, Dropdown, Button, Image } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
-// export class SingleCartItem extends Component {
-//     constructor(){
-//         super()
-//         this.state = {
-//             quantity: this.props.cartItem.quantity
-//         }
-//         this.handleChange = this.handleChange.bind(this)
-//         this.handleSubmit = this.handleSubmit.bind(this)
-//     }
+export class SingleCartItem extends Component {
+    constructor(){
+        super()
+        this.state = {
+            quantity: 0
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    componentDidMount() {
+        this.setState({
+            quantity: this.props.cartItem.quantity
+        })
+    }
 
-//     handleChange(event, data){
-//         console.log(data)
-//         // this.setState({
-//         //     quantity: event
-//         // })
+    handleChange(event, data){
+        this.setState({
+            quantity: data.value
+        })
+    }
 
-//     }
+    handleSubmit(event) {
+        event.preventDefault()
+        this.props.sendAddToCart({quantity: this.state.quantity-this.props.cartItem.quantity, productId: this.props.cartItem.productId, cartId: this.props.cartId}, true)
+    }
 
 
-//     render() {
-//         const cartItem = this.props.cartItem
-//         return (
-//             <List.Item>
-//                         <Image avatar src={cartItem.product.images[0].imageUrl}/>
-//                     <List.Content>
-//                             <List.Header as ={Link} to={`/products/${cartItem.product.id}`}>{cartItem.product.title}</List.Header>
-//                             <List.Description>Price: {cartItem.product.price} </List.Description>
-//                             <List.Description>Quantity:                             <Dropdown
-//                                 search
-//                                 placeholder={cartItem.quantity.toString()}
-//                                 selection
-//                                 compact
-//                                 options={options}
-//                                 onChange={this.handleChange}
-//                             />
+    render() {
+        let options = []
+        for(let i = 1; i <= 100; i++) {
+            options.push({text: i.toString(), value: i})
+        }
+        const cartItem = this.props.cartItem
+        return (
+            <List.Item>
+                        <Image avatar src={cartItem.product.images[0].imageUrl}/>
+                    <List.Content>
+                            <List.Header as ={Link} to={`/products/${cartItem.product.id}`}>{cartItem.product.title}</List.Header>
+                            <List.Description>Price: {cartItem.product.price} </List.Description>
+                            <List.Description>Quantity:                             <Dropdown
+                                search
+                                placeholder={cartItem.quantity.toString()}
+                                selection
+                                compact
+                                options={options}
+                                onChange={this.handleChange}
+                            />
 
-//                             </List.Description>
-//                         </List.Content>
-//                         <List.Content floated="right">
-//                             <Button onClick={() => this.props.sendAddToCart({quantity:1})} >Update</Button>
-//                         <Button onClick={() => this.props.removeItem(cartItem.id)}>Remove</Button>
-//                         </List.Content>
-//                     </List.Item>
-//         )
-//     }
-// }
+                            </List.Description>
+                        </List.Content>
+                        <List.Content floated="right">
+                            <Button onClick={this.handleSubmit} >Update</Button>
+                        <Button onClick={() => this.props.removeItem(cartItem.id)}>Remove</Button>
+                        </List.Content>
+                    </List.Item>
+        )
+    }
+}
 
-// const mapDispatchToProps = dispatch => ({
-//     removeItem: cartItemId => dispatch(removeCartItem(cartItemId)),
-//     sendAddToCart: (product, isUpdate) => dispatch(sendAddToCart(product, isUpdate))
-// })
+const mapStateToProps = state => ({
+    cartId: state.carts.cart.id
+})
 
-// export default connect(null, mapDispatchToProps)(SingleCartItem)
+const mapDispatchToProps = dispatch => ({
+    removeItem: cartItemId => dispatch(removeCartItem(cartItemId)),
+    sendAddToCart: (product, isUpdate) => dispatch(sendAddToCart(product, isUpdate))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(SingleCartItem)
 
 
 
@@ -74,10 +92,7 @@
 
 //     render() {
 
-//         let options = []
-//         for(let i = 1; i <= 100; i++) {
-//             options.push({text: i.toString(), value: i})
-//         }
+
 
 //         const {cartItems, user  } = this.props.cart;
 //         if (!this.props.cart.cartItems) return <p>Cart is Empty</p>

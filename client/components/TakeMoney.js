a5b5c7701e59895ea38645ac6cf3a038e4bca678
Mustafa Dane <mustafadane@gmail.com>
2018-11-06 22:13:09 -0600

    Deploying

--- client/components/TakeMoney.js
+++ client/components/TakeMoney.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import StripeCheckout from 'react-stripe-checkout';
+// import React from 'react';
+// import StripeCheckout from 'react-stripe-checkout';
 
-export default class TakeMoney extends React.Component {
-	onToken(token) {
-		fetch('/api/stripe', {
-			method: 'POST',
-			body: JSON.stringify({stripeToken: token.id})
-		}).then((response) => {
-			console.log(response);
-			response.json().then((data) => {
-				alert(`We are in business, ${data.email}`);
-			});
-		});
-	}
+// export default class TakeMoney extends React.Component {
+// 	onToken(token) {
+// 		fetch('/api/stripe', {
+// 			method: 'POST',
+// 			body: JSON.stringify({stripeToken: token.id})
+// 		}).then((response) => {
+// 			console.log(response);
+// 			response.json().then((data) => {
+// 				alert(`We are in business, ${data.email}`);
+// 			});
+// 		});
+// 	}
 
-	render() {
-		return <StripeCheckout name="YESTERNOW" token={this.onToken} stripeKey="pk_test_yvVC1NkMLli3EGIwWF0aR1BD" />;
-	}
-}
+// 	render() {
+// 		return <StripeCheckout name="YESTERNOW" token={this.onToken} stripeKey="pk_test_yvVC1NkMLli3EGIwWF0aR1BD" />;
+// 	}
+// }
 
 //publishable key pk_test_yvVC1NkMLli3EGIwWF0aR1BD
 //secret key sk_test_4uPct9rBJ9kVDIJb1hrT060R
--- server/api/stripe.js
+++ server/api/stripe.js
@@ -1,27 +1,27 @@
-// server/api/stripe
+// // server/api/stripe
 
-const router = require('express').Router();
-const {stripe} = require('../../secrets');
+// const router = require('express').Router();
+// const {stripe} = require('../../secrets');
 
-router.post('/', (req, res, next) => {
-	const stripeToken = req.body.stripeToken;
+// router.post('/', (req, res, next) => {
+// 	const stripeToken = req.body.stripeToken;
 
-	stripe.charges.create(
-		{
-			amount: 999,
-			currency: 'usd',
-			description: 'Example charge',
-			source: stripeToken
-		},
-		function(err, charge) {
-			console.log('charge: ', charge);
-			if (err) {
-				res.send({success: false, message: 'Error'});
-			} else {
-				res.send({success: true, message: 'Success'});
-			}
-		}
-	);
-});
+// 	stripe.charges.create(
+// 		{
+// 			amount: 999,
+// 			currency: 'usd',
+// 			description: 'Example charge',
+// 			source: stripeToken
+// 		},
+// 		function(err, charge) {
+// 			console.log('charge: ', charge);
+// 			if (err) {
+// 				res.send({success: false, message: 'Error'});
+// 			} else {
+// 				res.send({success: true, message: 'Success'});
+// 			}
+// 		}
+// 	);
+// });
 
-module.exports = router;
+// module.exports = router;
